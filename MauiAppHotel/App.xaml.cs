9c581a7047012a2545cffadd66824634d1e0710d
olivericky <ricardo_luiz16@hotmail.com>
2025-11-16 13:10:06 -0300

    Adicionar views e fonts.

--- MauiAppHotel/App.xaml.cs
+++ MauiAppHotel/App.xaml.cs
@@ -6,7 +6,18 @@
         {
             InitializeComponent();
 
-            MainPage = new AppShell();
+            MainPage = new NavigationPage(new Views.ContratacaoHospedagem());
         }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            var window = base.CreateWindow(activationState);
+
+            window.Width = 400;
+            window.Height = 600;
+
+            return window;
+        }
+
     }
 }
